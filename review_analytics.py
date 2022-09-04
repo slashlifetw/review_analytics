b59ce89bf98733aec183cccf71f1c8e8a91b6c5b
slashlifetw <dagancivil1931@gmail.com>
2022-09-04 16:30:22 +0800

    add calculation avg len of reivew

--- review_analytics.py
+++ review_analytics.py
@@ -1,6 +1,7 @@
 
 data = []
 count = 0
+total_num = 0
 
 with open('reviews.txt', 'r') as f:
     for line in f:
@@ -8,8 +9,12 @@ with open('reviews.txt', 'r') as f:
         count += 1 # count = count + 1
         if count % 1000 == 0: #假設count目前是1001去除1000餘數等於1，不等於0，所以不會印出來
             print(len(data))
-print(len(data))
+        total_num = total_num + len(line)
+        print(total_num)
+print('檔案讀取完畢，總共有', len(data), '筆資料')
+print('留言平均長度為', total_num / len(data))
 
-print(data[0])
-print('------------------')
-print(data[1])
+# total_num = 0
+# for str_number in data:
+#     total_num = total_num + len(str_number)
+# print('留言平均長度為', total_num / len(data))
